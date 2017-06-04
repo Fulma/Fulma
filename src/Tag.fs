9e7ee6dc5ed697de759465b4bec90d5f9de14ed1
Maxime Mangel <maxime.shadaen@protonmail.com>
2017-06-04 17:17:16 +0200

    Fix indentation

--- src/Tag.fs
+++ src/Tag.fs
@@ -10,14 +10,14 @@ open Fable.Helpers.React.Props
 
 module Tag =
 
-    [<StringEnum>]
-    type TagSize =
+  [<StringEnum>]
+  type TagSize =
     | [<CompiledName("is-medium")>] Medium
     | [<CompiledName("is-large")>]Large
-    | [<CompiledName("")>] None
+    | [<CompiledName("")>] Normal
 
-    [<StringEnum>]
-    type TagColor =
+  [<StringEnum>]
+  type TagColor =
     | [<CompiledName("is-black")>] Black
     | [<CompiledName("is-dark")>] Dark
     | [<CompiledName("is-light")>] Light
@@ -29,36 +29,31 @@ module Tag =
     | [<CompiledName("is-danger")>] Danger
     | [<CompiledName("")>] None
 
+  type Option =
+  | Size of TagSize
+  | Color of TagColor
 
-    type Option =
-    | Size of TagSize
-    | Color of TagColor
+  type Options =
+    { size: TagSize
+      color : TagColor }
 
-    type Options =
-        {
-            size: TagSize
-            color : TagColor
-        }
+    static member Empty =
+      { size = TagSize.Normal
+        color = TagColor.None }
 
-        static member Empty =
-            {
-                size = TagSize.None
-                color = TagColor.None
-            }
+  let tag (options: Option list) (properties: IHTMLProp list) children =
+    let parseOption result opt =
+      match opt with
+      | Size s ->
+          { result with size = s }
+      | Color c ->
+          { result with color = c }
 
-    let tag (options: Option list) (properties: IHTMLProp list) children =
-        let parseOption result opt =
-            match opt with
-            | Size s ->
-                {result with size = s}
-            | Color c ->
-                {result with color = c}
+    let opts = options |> List.fold parseOption Options.Empty
 
-        let opts = options |> List.fold parseOption Options.Empty
+    let className =
+      ClassName (sprintf "tag %s %s" (unbox<string>opts.size) (unbox<string>opts.color) )
 
-        let className =
-          ClassName (sprintf "tag %s %s" (unbox<string>opts.size) (unbox<string>opts.color) )
-
-        span
-          ((className :> IHTMLProp) :: properties)
-          children
+    span
+      ((className :> IHTMLProp) :: properties)
+      children
