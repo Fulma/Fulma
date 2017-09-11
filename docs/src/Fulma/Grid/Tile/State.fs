70a1587cb4260b0dd3351a91159189b2612d32e0
Maxime Mangel <maxime.shadaen@protonmail.com>
2017-09-11 21:49:41 +0200

    Add tile documentation

--- docs/src/Fulma/Grid/Tile/State.fs
+++ docs/src/Fulma/Grid/Tile/State.fs
@@ -6,20 +6,64 @@ open Types
 let iconCode =
     """
 ```fsharp
-Box.box' [ ]
-    [ str "Lorem ipsum dolor sit amet, consectetur adipisicing elit
-           , sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." ]
+    Tile.ancestor [ ]
+        [ Tile.parent [ Tile.isVertical
+                        Tile.is4 ]
+            [ Tile.child [ ]
+                [ Box.box' [ ]
+                    [ Heading.p [ ]
+                        [ str "Tile n°1" ]
+                      p [ ]
+                        [ str "Nulla at urna iaculis, eleifend dolor eget, pellentesque eros. Mauris luctus pharetra velit, viverra feugiat nibh vehicula vitae. Suspendisse vitae sem id." ] ] ]
+              Tile.child [ ]
+                [ Box.box' [ ]
+                    [ Heading.p [ ]
+                        [ str "Tile n°2" ]
+                      p [ ]
+                        [ str "Curabitur pretium nisi tortor, vitae elementum justo blandit sit amet. Pellentesque vel commodo metus. In scelerisque pretium quam, quis varius lectus maximus sed." ] ] ]
+            ]
+          Tile.parent [ ]
+            [ Tile.child [ ]
+                [ Box.box' [ Box.props [ Style [ Height "100%" ] ] ]
+                    [ Heading.p [ ]
+                        [ str "Tile n°3" ]
+                      p [ ]
+                        [ str "Etiam quis neque efficitur, iaculis urna eget, efficitur ligula. Cras faucibus, magna eu eleifend maximus, ligula ex gravida libero, vitae suscipit velit nibh eget eros." ]
+                      p [ ]
+                        [ str "Suspendisse vel turpis nisi. Fusce at risus accumsan, varius massa id, dictum est. Aenean consequat neque sed tincidunt eleifend." ]
+                      p [ ]
+                        [ str "Phasellus ac lectus in ex condimentum sollicitudin. Sed id mollis turpis. Sed at felis vel diam interdum viverra." ] ] ] ] ]
 ```
     """
 
 let init() =
     { Intro =
         """
-# Box
+# Tile
 
-A white **box** to contain other elements .
+A **single tile** element to build 2-dimensional Metro-like, Pinterest-like, or whatever-you-like grids
 
-*[Bulma documentation](http://bulma.io/documentation/elements/box/)*
+*[Bulma documentation](http://bulma.io/documentation/layout/tiles/)*
+
+**Important**
+
+In Bulma, you would generally apply the `tile` class directly to your components.
+
+```html
+<div class="title box">
+</div>
+```
+
+However with Fulma, we can't provide you a wrapper to work that way. So instead, we create a "tile div" and you place you child in it.
+
+```html
+<div class="tile">
+    <div class="box"></div>
+</div>
+```
+
+This is important because you will probably need to add `style="height: 100%` to your child element to make it take the whole tile height.
+
         """
       BoxViewer = Viewer.State.init iconCode }
 
--- docs/src/Fulma/Grid/Tile/View.fs
+++ docs/src/Fulma/Grid/Tile/View.fs
@@ -10,10 +10,33 @@ open Fulma.Grids
 
 
 let iconInteractive =
-    div [ ClassName "block" ]
-        [ Box.box' [ ]
-            [ str "Lorem ipsum dolor sit amet, consectetur adipisicing elit
-                   , sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."] ]
+    Tile.ancestor [ ]
+        [ Tile.parent [ Tile.isVertical
+                        Tile.is4 ]
+            [ Tile.child [ ]
+                [ Box.box' [ ]
+                    [ Heading.p [ ]
+                        [ str "Tile n°1" ]
+                      p [ ]
+                        [ str "Nulla at urna iaculis, eleifend dolor eget, pellentesque eros. Mauris luctus pharetra velit, viverra feugiat nibh vehicula vitae. Suspendisse vitae sem id." ] ] ]
+              Tile.child [ ]
+                [ Box.box' [ ]
+                    [ Heading.p [ ]
+                        [ str "Tile n°2" ]
+                      p [ ]
+                        [ str "Curabitur pretium nisi tortor, vitae elementum justo blandit sit amet. Pellentesque vel commodo metus. In scelerisque pretium quam, quis varius lectus maximus sed." ] ] ]
+            ]
+          Tile.parent [ ]
+            [ Tile.child [ ]
+                [ Box.box' [ Box.props [ Style [ Height "100%" ] ] ]
+                    [ Heading.p [ ]
+                        [ str "Tile n°3" ]
+                      p [ ]
+                        [ str "Etiam quis neque efficitur, iaculis urna eget, efficitur ligula. Cras faucibus, magna eu eleifend maximus, ligula ex gravida libero, vitae suscipit velit nibh eget eros." ]
+                      p [ ]
+                        [ str "Suspendisse vel turpis nisi. Fusce at risus accumsan, varius massa id, dictum est. Aenean consequat neque sed tincidunt eleifend." ]
+                      p [ ]
+                        [ str "Phasellus ac lectus in ex condimentum sollicitudin. Sed id mollis turpis. Sed at felis vel diam interdum viverra." ] ] ] ] ]
 
 let root model dispatch =
     Render.docPage [ Render.contentFromMarkdown model.Intro
