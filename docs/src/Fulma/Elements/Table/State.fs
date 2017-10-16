bc643bbca71b09562e45ee5a06e227ea4a222417
Edouard Moureaux <edouard.moureaux@gmail.com>
2017-10-16 21:47:53 +0200

    Updates docs to display Table.isFullWidth

--- docs/src/Fulma/Elements/Table/State.fs
+++ docs/src/Fulma/Elements/Table/State.fs
@@ -55,6 +55,33 @@ let modifierCode =
                    td [ ] [ str "11/07/1978" ] ] ] ]
 ```
     """
+let modifierFullWidthCode =
+    """
+```fsharp
+    Table.table [ Table.isBordered
+                  Table.isFullWidth
+                  Table.isStripped ]
+        [ thead [ ]
+            [ tr [ ]
+                 [ th [ ] [ str "Firstname" ]
+                   th [ ] [ str "Surname" ]
+                   th [ ] [ str "Birthday" ] ] ]
+          tbody [ ]
+            [ tr [ ]
+                [ td [ ] [ str "Maxime" ]
+                  td [ ] [ str "Mangel" ]
+                  td [ ] [ str "28/02/1992" ] ]
+              tr [ Table.Row.isSelected ]
+                 [ td [ ] [ str "Jane" ]
+                   td [ ] [ str "Doe" ]
+                   td [ ] [ str "21/07/1987" ] ]
+              tr [  ]
+                 [ td [ ] [ str "John" ]
+                   td [ ] [ str "Doe" ]
+                   td [ ] [ str "11/07/1978" ] ] ] ]
+```
+    """
+
 
 let init() =
     { Intro =
@@ -64,7 +91,8 @@ let init() =
 *[Bulma documentation](http://bulma.io/documentation/elements/table/)*
         """
       SimpleViewer = Viewer.State.init simpleCode
-      ModifierViewer = Viewer.State.init modifierCode }
+      ModifierViewer = Viewer.State.init modifierCode
+      ModifierFullWidth = Viewer.State.init modifierFullWidthCode }
 
 let update msg model =
     match msg with
@@ -75,3 +103,7 @@ let update msg model =
     | ModifierViewerMsg msg ->
         let (viewer, viewerMsg) = Viewer.State.update msg model.ModifierViewer
         { model with ModifierViewer = viewer }, Cmd.map ModifierViewerMsg viewerMsg
+
+    | ModifierFullWidthMsg msg ->
+        let (viewer, viewerMsg) = Viewer.State.update msg model.ModifierFullWidth
+        { model with ModifierFullWidth = viewer }, Cmd.map ModifierFullWidthMsg viewerMsg
