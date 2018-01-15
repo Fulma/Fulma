0dace4da1c9eadc11227b45df2d2db85c7843f59
Alfonso Garcia-Caro <alfonso.garcia-caro@hotmail.com>
2018-01-15 22:53:12 +0100

    Plugin: Inject text into member response

--- docs/Plugins/Fable.Plugins.ImportExample.fs
+++ docs/Plugins/Fable.Plugins.ImportExample.fs
@@ -7,22 +7,27 @@ open Fable.AST
 open Fable2Babel
 
 type ImportExamplePlugins() =
+    // interface IReplacePlugin with
+    //     member __.TryReplace _com (info: Fable.ApplyInfo) =
+    //         match info.ownerType, info.methodName with
+    //         | Fable.DeclaredType(ent,_), "renderView" ->
+    //             let args = failwith "MY CUSTOM ARGS"
+    //             let owner = Fable.Value(Fable.TypeRef(ent,[]))
+    //             Some <| Fable.Apply(owner, args, Fable.ApplyMeth, info.returnType, info.range)
+    //         | _ -> None
     interface IDeclarePlugin with
+
         member __.TryDeclareRoot _com _ctx _file =
             None
         member __.TryDeclare _com ctx decl =
             match decl with
             | Fable.MemberDeclaration(m,_,_,_,_,Some r) ->
                 if m.Decorators |> List.exists (fun x -> x.Name.Contains("ExampleCode")) then
-                    printfn "===================="
-                    printfn "EXAMPLE: %s" m.Name
-                    File.ReadLines(ctx.file.SourcePath)
-                    |> Seq.skip r.start.line
-                    |> Seq.takeWhile (String.IsNullOrWhiteSpace >> not)
-                    |> Seq.iter (printfn "%s")
-                    printfn "===================="
-                    // transformTest com ctx (test, decorator, args, body, range)
-                    // |> List.singleton |> Some
-                    Some [Babel.ExpressionStatement(Babel.NullLiteral())]
+                    let text =
+                        File.ReadLines(ctx.file.SourcePath)
+                        |> Seq.skip r.start.line
+                        |> Seq.takeWhile (String.IsNullOrWhiteSpace >> not)
+                        |> String.concat "\n"
+                    Some [Babel.ReturnStatement(Babel.StringLiteral text)]
                 else None
             | _ -> None
