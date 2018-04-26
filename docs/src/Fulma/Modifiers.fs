b53b43c1d2cdad85752931bf5b493a644f932a70
selketjah <gien@eightpointsquared.com>
2018-04-26 11:37:00 +0200

    add sizes (text + example) to modifier, implement customcolor example

--- docs/src/Fulma/Modifiers.fs
+++ docs/src/Fulma/Modifiers.fs
@@ -1,32 +1,13 @@
 module Fulma.Modifiers
 
-let view =
-    Render.contentFromMarkdown
-        """
-# Modifiers
+open Fable.Helpers.React
+open Fable.Helpers.React.Props
+open Fulma
+open Fulma.Extensions
 
-Fulma currently provide modifier support for `Colors` and `Sizes`.
-
-## Colors
-
-
-### Supported colors
-
-Current colors are supported: `white`, `black`, `light`, `dark`, `primary`, `info`, `success`, `warning`, `danger`.
-
-Follow elements can have color attribute:
-* [Button](#fulma/elements/button),
-* [Notification](#fulma/elements/notification),
-* [Progress](#fulma/elements/progress),
-* [Tag](#fulma/elements/tag)
-
-### Custom colors
-
-```fsharp
 let inline isFrontsPoly<'a> = IsCustomColor "poly-fronts"
 let inline isInstallationsPoly<'a> = IsCustomColor "poly-installations"
-
-let private viewFilters =
+let customColor() =
     Field.div [ Field.IsGrouped ]
         [ yield! Checkradio.checkboxInline [ Checkradio.Color isInstallationsPoly
                                              Checkradio.HasBackgroundColor ]
@@ -36,7 +17,39 @@ let private viewFilters =
                 [ str "Fronts" ]
           yield! Checkradio.checkboxInline [ ]
                 [ str "Zones fixes" ] ]
-```
+
+let textSize() =
+    div [ ClassName "block" ]
+        [ Content.content [ Content.Size IsSmall ] [ str "Small" ]
+          Content.content [ ] [ str "Normal" ]
+          Content.content [ Content.Size IsMedium ] [ str "Medium" ]
+          Content.content [ Content.Size IsLarge ] [ str "Large" ] ]
+
+let view =
+    Render.docPage [
+        Render.contentFromMarkdown
+            """
+# Modifiers
+
+Fulma currently provide modifier support for `Colors` and `Sizes`.
+
+## Colors
+
+Color is a modifier that can be added to elements.
+
+### Supported colors
+
+Current colors are supported: `white`, `black`, `light`, `dark`, `primary`, `info`, `success`, `warning`, `danger`.
+
+Following elements support the color modifier:
+* [Button](#fulma/elements/button),
+* [Notification](#fulma/elements/notification),
+* [Progress](#fulma/elements/progress),
+* [Tag](#fulma/elements/tag).
+
+### Custom colors
+
+You can add your own colors
 
 ```css
 @import "./../../node_modules/bulma/sass/utilities/initial-variables";
@@ -57,12 +70,22 @@ $addColors: (
 $colors: map-merge($colors, $addColors);
 ```
 
+Now you are ready to use your custom colors in your code:
+
+        """
+        Render.docSection
+            "### Customer Colors"
+            (Widgets.Showcase.view customColor (Render.getViewSource customColor))
+        Render.docSection
+            """
 ### No colors
 
 
 ## Sizes
 
-TODO: Text about supported sizes
+Size is a modifier that you can add to your layout, elements or components.
+Current sizes are supported: `small`, `medium` and `large`.
 
 
-        """
+            """
+            (Widgets.Showcase.view textSize (Render.getViewSource textSize))]
