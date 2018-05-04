8bfb5a80d7764232fe39e3e12a8b6a17cde5f339
selketjah <gien@eightpointsquared.com>
2018-05-04 22:43:48 +0200

    add modifiers to media component

--- src/Fulma/Components/Media.fs
+++ src/Fulma/Components/Media.fs
@@ -19,15 +19,18 @@ module Media =
         | Size of ISize
         | Props of IHTMLProp list
         | CustomClass of string
+        | Modifiers of IModifier list
 
     type internal Options =
         { Size : string option
           Props : IHTMLProp list
-          CustomClass : string option }
+          CustomClass : string option
+          Modifiers : string option list }
         static member Empty =
             { Size = None
               Props = []
-              CustomClass = None }
+              CustomClass = None
+              Modifiers = [] }
 
     /// Generate <article class="media"></article>
     let media (options: Option list) children =
@@ -40,28 +43,29 @@ module Media =
             | Size size -> { result with Size = ofSize size |> Some }
             | Props props -> { result with Props = props }
             | CustomClass customClass -> { result with CustomClass = customClass |> Some }
+            | Modifiers modifiers -> { result with Modifiers = modifiers |> parseModifiers }
 
         let opts = options |> List.fold parseOption Options.Empty
         let classes = Helpers.classes
                         Classes.Container
-                        [ opts.Size; opts.CustomClass ]
+                        (opts.Size::opts.CustomClass::opts.Modifiers)
                         [ ]
         article (classes::opts.Props) children
 
     /// Generate <div class="media-left"></div>
     let left (options: GenericOption list) children =
         let opts = genericParse options
-        let classes = Helpers.classes Classes.Left [opts.CustomClass] []
+        let classes = Helpers.classes Classes.Left (opts.CustomClass::opts.Modifiers) []
         figure (classes::opts.Props) children
 
     /// Generate <div class="media-right"></div>
     let right (options: GenericOption list) children =
         let opts = genericParse options
-        let classes = Helpers.classes Classes.Right [opts.CustomClass] []
+        let classes = Helpers.classes Classes.Right (opts.CustomClass::opts.Modifiers) []
         div (classes::opts.Props) children
 
     /// Generate <div class="media-content"></div>
     let content (options: GenericOption list) children =
         let opts = genericParse options
-        let classes = Helpers.classes Classes.Content [opts.CustomClass] []
+        let classes = Helpers.classes Classes.Content (opts.CustomClass::opts.Modifiers) []
         div (classes::opts.Props) children
