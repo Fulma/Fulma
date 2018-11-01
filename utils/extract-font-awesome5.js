2bf9c00425317d64733b288cff6c3567e1f18bc4
Gien Verschatse <gien_verschatse@hotmail.com>
2018-11-01 13:41:18 +0100

    update extract fontawesome5 script to fetch all solid icons and brands

--- utils/extract-font-awesome5.js
+++ utils/extract-font-awesome5.js
@@ -1,46 +1,66 @@
-// Go to https://fontawesome.com/cheatsheet
+// Go to https://fontawesome.com/cheatsheet/pro
 // Copy & Paste the following code into the console
 // You will get a textarea will all the icons clases converted into an DU
 // Copy & Paste the DU into the right file
 
 //select solid, regular and brands sections
-var rawData = Array.prototype.slice.call(document.querySelectorAll(".cheatsheet-set"));
+var iconsSection = Array.prototype.slice.call(document.querySelectorAll("#solid"));
+var brandsSection = Array.prototype.slice.call(document.querySelectorAll("#brands"));
 
-var sets = [];
+var icons = [];
+var brands = [];
 
-rawData.forEach(element => {
+brandsSection.forEach(element => {
     var articles = element.querySelectorAll('article');
+    articles.forEach(icon => {
+        brands.push(icon.getAttribute('id'));
+    });
+});
 
-    var icons = [];
+iconsSection.forEach(element => {
+    var articles = element.querySelectorAll('article');
     articles.forEach(icon => {
         icons.push(icon.getAttribute('id'));
     });
-
-    sets.push([element.getAttribute('id'), icons]);
 });
 
 // Extract only the class name
-var cleaned =
-sets
-    .map(function (set) {
-        var name = set[0];
-        var icons = set[1];
-        return `[<StringEnum>]
-            type ` + name.charAt(0).toUpperCase() + name.slice(1) + ` =
-                |` +
-                icons
-                    .map(function (icon) {
-                        var compileName = 'fa' + name.charAt(0) + ' fa-' + icon
-                        return ` [<CompiledName("${compileName}")>] ` + icon
-                            .split("-")
-                            .map(function (element) {
-                                return element.replace(/\b\w/g, function (l) { return l.toUpperCase() })
-                            }, this)
-                            .join("") + " ";
-                    }, this)
-                    .join(`\n    |`);
+var brandsDu =
+`[<StringEnum>]
+type FontAwesomeBrands =
+    |` +
+    brands
+    .map(function (element) {
+        var compileName = 'fa-' + element
+        return ` [<CompiledName("${compileName}")>] ` + element
+            .split("-")
+            .map(function (element) {
+                return element.replace(/\b\w/g, function (l) { return l.toUpperCase() })
+            }, this)
+            .join("") + " ";
     }, this)
-    .join(`\n\n    `);
+    .join(`\n    |`);
+
+var iconsDu =
+    `[<StringEnum>]
+    type FontAwesomeIcons =
+        |` +
+        icons
+        .map(function (element) {
+            var compileName = 'fa-' + element
+            return ` [<CompiledName("${compileName}")>] ` + element
+                .split("-")
+                .map(function (element) {
+                    return element.replace(/\b\w/g, function (l) { return l.toUpperCase() })
+                }, this)
+                .join("") + " ";
+        }, this)
+        .join(`\n    |`);
+
+var cleaned =
+    iconsDu +
+    `\n ` +
+    brandsDu;
 
 var textarea = document.createElement("textarea");
 textarea.style.width = "100%";
