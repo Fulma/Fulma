ba3e4f69f243155df8c8dbc1271ecf711e221c8d
Maxime Mangel <maxime.shadaen@protonmail.com>
2018-12-03 12:58:05 +0100

    Bulma 0.7.2: Add ol type support to content

--- src/Fulma/Elements/Content.fs
+++ src/Fulma/Elements/Content.fs
@@ -9,6 +9,11 @@ module Content =
 
     module Classes =
         let [<Literal>] Container = "content"
+        module Ol =
+            let [<Literal>] IsLowerRoman = "is-lower-roman"
+            let [<Literal>] IsUpperRoman = "is-upper-roman"
+            let [<Literal>] IsLowerAlpha = "is-lower-alpha"
+            let [<Literal>] IsUpperAlpha = "is-upper-alpha"
 
     type Option =
         | Size of ISize
@@ -47,3 +52,48 @@ module Content =
         div
             (classes::opts.Props)
             children
+
+    module Ol =
+
+        type Option =
+            | IsLowerRoman
+            | IsUpperRoman
+            | IsLowerAlpha
+            | IsUpperAlpha
+            | CustomClass of string
+            | Modifiers of Modifier.IModifier list
+            | Props of IHTMLProp list
+
+        type internal Options =
+            { Style : string option
+              Props : IHTMLProp list
+              CustomClass : string option
+              Modifiers : string option list }
+
+            static member Empty =
+                { Style = None
+                  Props = []
+                  CustomClass = None
+                  Modifiers = [] }
+
+        let ol (options : Option list) children =
+            let parseOption (result : Options) opt =
+                match opt with
+                | IsLowerRoman -> { result with Style = Some Classes.Ol.IsLowerRoman }
+                | IsUpperRoman -> { result with Style = Some Classes.Ol.IsUpperRoman }
+                | IsLowerAlpha -> { result with Style = Some Classes.Ol.IsLowerAlpha }
+                | IsUpperAlpha -> { result with Style = Some Classes.Ol.IsUpperAlpha }
+                | Props props -> { result with Props = props }
+                | CustomClass customClass -> { result with CustomClass = Some customClass }
+                | Modifiers modifiers -> { result with Modifiers = modifiers |> Modifier.parseModifiers }
+
+            let opts = options |> List.fold parseOption Options.Empty
+            let classes = Helpers.classes
+                            Classes.Container
+                            ( opts.Style
+                              ::opts.CustomClass
+                              ::opts.Modifiers )
+                            [ ]
+
+            ol (classes::opts.Props)
+               children
