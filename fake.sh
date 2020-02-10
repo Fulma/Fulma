1ba6116ab9eb3bf125d1334db5618b30924c6625
Maxime Mangel <mangel.maxime@protonmail.com>
2020-02-10 13:50:51 +0000

    Use dotnet tools for paket and fake + fix dependencies

--- fake.sh
+++ fake.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-OS=${OS:-"unknown"}
-
-echo $OSTYPE
-if [ "$OS" != "Windows_NT" ]
-then
-  # Allows NETFramework like net45 to be built using dotnet core tooling with mono
-  export FrameworkPathOverride=$(dirname $(which mono))/../lib/mono/4.5/
-fi
-
-dotnet restore build.proj
+dotnet tool restore
 dotnet fake $@
